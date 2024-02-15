3ab82facca3919422ee1b0a748c3d0d7dc1d3036
Ben Bld <bld.prom@gmail.com>
2024-02-15 14:52:20 +0100

    chore: set up index

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./sass/main.scss";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { User } from "./pages/user";
+
+const App = () => {
+  // useEffect(() => {
+  //   // Appelle la fonction de test lorsque le composant est monté
+  //   testFetchFunctions();
+  // }, []);
+
+  return (
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/user/18" />} />
+          <Route path="/user/:id" element={<User />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  );
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
